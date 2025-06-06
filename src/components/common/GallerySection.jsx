5d8125c999199744b20399c1d0e486da204d4cff
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-06-07 00:09:24 +0530

    feat: responsive

--- src/components/common/GallerySection.jsx
+++ src/components/common/GallerySection.jsx
@@ -11,21 +11,21 @@ import WhiteGlowDiv from './WhiteGlowDiv';
 
 const GallerySection = () => {
   return (
-    <section className="py-12 px-[5rem] sm:py-16 lg:py-20 bg-gradient-gallery">
+    <section className="py-12 px-[1rem] lg:px-[5rem] sm:py-16 lg:py-20 bg-gradient-gallery">
       <div className="max-w-7xl mx-auto">
-        <div className="text-center mb-28">
+        <div className="text-center sm:mb-28">
           <WhiteGlowDiv text="Gallery" />
 
-          <p className="text-3xl sm:text-4xl lg:text-[2.20rem] text-[#2C2C2C] font-urbanist font-extrabold text-center mb-4">
+          <p className="text-[1.25rem] sm:text-4xl lg:text-[2.20rem] text-[#2C2C2C] font-urbanist font-extrabold text-center sm:mb-4 leading-[1.5rem]">
             A glimpse into the lives you have
           </p>
-          <p className="text-3xl sm:text-4xl lg:text-[2.20rem] text-[#2C2C2C]  font-urbanist font-extrabold  text-center mb-8">
+          <p className="text-[1.25rem] sm:text-4xl lg:text-[2.20rem] text-[#2C2C2C]  font-urbanist font-extrabold text-center mb-8 leading-[1.5rem]">
             touched with your{' '}
             <span className="text-accent-yellow [font-style:oblique_6deg]">kindness.</span>
           </p>
         </div>
 
-        <div className="flex justify-evenly items-start space-x-8">
+        <div className="hidden sm:flex justify-evenly items-start space-x-8">
           {/* Left Column */}
           <div className="space-y-12">
             <div className="overflow-hidden bg-white rounded-[20px] p-0 group shadow-[15px_15px_38px_rgba(209,213,215,0.9)]">
@@ -54,14 +54,14 @@ const GallerySection = () => {
               />
             </div>
 
-            <div className="text-center py-auto flex flex-col justify-evenly space-y-4 lg:py-12">
-              <p className="text-2xl sm:text-3xl lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] leading-tight">
+            <div className="text-center py-auto flex flex-col justify-evenly md:space-y-3 lg:space-y-4 md:py-8 lg:py-12">
+              <p className="text-[1.75rem] sm:text-3xl md:text-[1.75rem] lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] lg:leading-[1rem]">
                 Real change doesn't
               </p>
-              <p className="text-2xl sm:text-3xl lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] leading-tight">
+              <p className="text-[1.75rem] sm:text-3xl md:text-[1.75rem] lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] lg:leading-[1.5rem]">
                 always speak
               </p>
-              <p className="text-2xl sm:text-3xl lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] leading-tight">
+              <p className="text-[1.75rem] sm:text-3xl md:text-[1.75rem] lg:text-[2rem] font-urbanist font-bold text-[#2C2C2C] lg:leading-[1.5rem]">
                 it shows. <span className="text-accent-yellow font-extrabold">Just like this.</span>
               </p>
             </div>
@@ -84,6 +84,12 @@ const GallerySection = () => {
               />
             </div>
           </div>
+        </div>
+        {/* Mobile View */}
+        <div className='flex sm:hidden flex-col space-y-5 items-center px-[4rem]'>
+          <img className='rounded-[2rem]' src={galleryImages[1].src} alt={galleryImages[1].alt}/>
+          <img className='rounded-[2rem]' src={galleryImages[4].src} alt={galleryImages[1].alt}/>
+          <img className='rounded-[2rem]' src={galleryImages[0].src} alt={galleryImages[0].alt}/>
         </div>
       </div>
     </section>
