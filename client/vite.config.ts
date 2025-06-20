28e1d1a49c0bbde058823f864f1cea6ca41ce5d7
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-06-21 00:07:21 +0530

    refactor: update dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'; // faster builds than @vitejs/plugin-react
 import path from 'path';
-import tagger from '@dhiwise/component-tagger';
-// import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,12 +8,9 @@ export default defineConfig(({ mode }) => ({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new'],
   },
   plugins: [
     react(),
-    tagger(), // from JSX project
-    // mode === 'development' && componentTagger(), // from TSX project
   ].filter(Boolean),
   build: {
     outDir: 'build',
