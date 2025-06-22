56ec9f952d694863f23dffa7187845ae1091c9ba
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-06-22 13:15:22 +0530

    feat indexing status

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -3,13 +3,15 @@ import mongoose from 'mongoose';
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, default: 'Hare Krishna Vidya Team' },
-  date: { type: Date, default: Date.now },  // 👈 sets current date by default
+  date: { type: Date, default: Date.now }, 
   status: { type: String, enum: ['Draft', 'Published'], default: 'Draft' },
   content: { type: String, required: true },
   excerpt: { type: String },
   image: { type: String },
   category: { type: String }
 });
+
+blogSchema.index({ status: 1 });
 
 const Blog = mongoose.model('Blog', blogSchema);
 
