894508a540887b96f853258e2027956317fa1716
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-06-28 13:16:48 +0530

    update: trust proxy to 1

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(express.json({ limit: process.env.PAYLOAD_LIMIT || '1mb' }));
 
 // Security middlewares
 app.use(helmet());
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 // custom XSS Sanitizer
 app.use((req, res, next) => {
